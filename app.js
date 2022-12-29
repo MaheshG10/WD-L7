c6091e7559e7ed77cb953e82c0f67e421ce18c33
MaheshG10 <92197783+MaheshG10@users.noreply.github.com>
2022-12-29 21:50:27 +0530

    final commite

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
+/* eslint-disable no-unused-vars */
+const { request, response } = require("express");
 const express = require("express");
 const app = express();
 var csrf = require("tiny-csrf");
+
+const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
-app.use(bodyParser.json());
-const path = require("path");
-const { Todo } = require("./models");
-// eslint-disable-next-line no-unused-vars
-const todo = require("./models/todo");
-app.use(express.static(path.join(__dirname, "public")));
+
 app.use(express.urlencoded({ extended: false }));
+const path = require("path");
+
+app.use(bodyParser.json());
 app.use(cookieParser("shh! some secret string"));
 app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
+// eslint-disable-next-line no-unused-vars
+const todo = require("./models/todo");
+// app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
 app.get("/", async (request, response) => {
@@ -31,30 +36,38 @@ app.get("/", async (request, response) => {
       csrfToken: request.csrfToken(),
     });
   } else {
-    response.json(overdue, dueLater, dueToday, completedItems);
+    response.json(allTodos, overdue, dueToday, dueLater);
   }
 });
 
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/todos", async (request, response) => {
-  // defining route to displaying message
-  console.log("Todo list");
+  console.log("Todo List", request.body);
+});
+app.post("/todos", async (request, response) => {
+  console.log("Todo List");
   try {
-    const todoslist = await Todo.findAll();
-    return response.json(todoslist);
+    console.log("entering in try block");
+    const todo = await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+    });
+    return response.redirect("/");
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
   }
 });
-app.get("/todos/:id", async function (request, response) {
-  try {
-    const todo = await Todo.findByPk(request.params.id);
-    return response.json(todo);
-  } catch (error) {
-    console.log(error);
-    return response.status(422).json(error);
-  }
-});
+
+// app.get("/todos/:id", async function (request, response) {
+//   try {
+//     const todo = await Todo.findByPk(request.params.id);
+//     return response.json(todo);
+//   } catch (error) {
+//     console.log(error);
+//     return response.status(422).json(error);
+//   }
+// });
 
 app.post("/todos", async (request, response) => {
   console.log("creating new todo", request.body);
